bdc4a65ab98b43f204652825e4ced662c021440e
jasonburnell98 <50059791+jasonburnell98@users.noreply.github.com>
2019-11-07 09:11:50 -0500

    rough database updated

---
+++ custom.js
@@ -0,0 +1,75 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBUjU1OuJUnvueOI9FSvPGnnAg5w3uBQog",
+    authDomain: "ourreddit-1572579679058.firebaseapp.com",
+    databaseURL: "https://ourreddit-1572579679058.firebaseio.com",
+    projectId: "ourreddit-1572579679058",
+    storageBucket: "ourreddit-1572579679058.appspot.com",
+    messagingSenderId: "759250034016",
+    appId: "1:759250034016:web:612e3bb641ced9e02c1205",
+    measurementId: "G-64PCTQ8QLZ"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+ 
+  //firebase.database().ref("hi").set("there");
+
+  
+            //make auth and firestone references
+            const auth = firebase.auth();
+            // const db = firebase.firestore();
+
+
+  //This is a silly andy trick, ignore or not
+//   var myid = "";
+//   if(localStorage) {
+//     if(!localStorage.getItem("sillyID")) {
+//         myid = "id"+Math.floor(Math.random()*500000000);
+//         localStorage.setItem("sillyID", myid);
+//     } else {
+//       myid = localStorage.getItem("sillyID");
+//     }
+//   }
+
+
+    //Two views: 1. list of examples
+    //2. single page app
+    let clickHandler = function(evt){
+        let gameid = $(evt.currentTarget).attr("data-gameid");
+        displayGame(gameid);   
+    }
+
+    let displayLobby = function(){
+        $("#mainscreen").html($("#mytemplate").html());
+        firebase.database().ref("lobby").on("value", ss=>{
+                let gamesObj = ss.val();
+                let gameids = Object.keys(gamesObj);
+                $("thegames").html('');
+                gameids.map(gameid=>{
+                    $("#thegames").append(`
+                    <li>
+                        <a class="showgame" data-gameid=${gameid}>${gamesObj[gameid].title}
+                        </a>
+                        </li> `);
+                });
+                $(".showgame").off("click",clickHandler);
+                $(".showgame").on("click",clickHandler);
+        });
+    $("#createExample").on("click",function(){
+        let newExampleRef = firebase.database().ref("lobby").push();
+        newExampleRef.set({title: $("#examplename").val()});
+    })
+
+
+
+    }
+    let displayGame = function(gameid){
+        alert("1");
+        $("#mainscreen").html(`
+        <h1>Welcome to the ${gameid}</h1>
+        <button class = "backbutton">Back to Lobby</button>
+        <ul id="users"> </ul>
+        `);
+        $(".backbutton").on("click",displayLobby);
+    }
+    
+    displayLobby();
