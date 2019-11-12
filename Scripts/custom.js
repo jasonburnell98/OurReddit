70bfda8bafe6c16e6239c562b3c8bbd86d672d58
Shabir Ghulam <sghulam@udel.edu>
2019-11-11 21:06:42 -0500

    fixed comments Fully working

--- Scripts/custom.js
+++ Scripts/custom.js
@@ -1,15 +1,19 @@
 var createComment=0;
 var post_flag=0;
 
-var postToComment;
 
+
+    //function that creates a post
   function add()
         {
             var title = $("#name").val();
             var content = $("#contents").val();
             var today = new Date();
+            
             var currdate = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-
+            var today = new Date();
+            var time = today.getHours()%12 + ":" + today.getMinutes() + ":" + today.getSeconds();
+            currdate= currdate +", "+time;
            db.collection("posts").add({
                
                 title: title,
@@ -44,23 +48,30 @@ var postToComment;
             //  });
 
 
+    //handles when clicked on a post
     let clickHandler = function(evt){
         let postid = $(evt.currentTarget).attr("data-postid");
         displaySinglePost(postid);   
     }
 
+    //handles when comments get clicked
     let commentclickHandler = function(evt){
         let postid = $(evt.currentTarget).attr("data-commentid");
         displaySinglecomment(postid);   
     }
 
+    let createCommentClickHandler = function(evt){
+        let postid = $(evt.currentTarget).attr("data-commentid");
+        create_comment(postid);   
+    }
+
     //every time a change is made, a new post is added as an li
+    //this function displays all the posts
     function displayPosts()
     {
         db.collection("posts").get().then(function(querySnapshot) {
             $("#theposts").html('');
             querySnapshot.forEach(function(doc) {
-                // doc.data() is never undefined for query doc snapshots
                 var data = doc.data();
                 var id = doc.id;
                 console.log(data.country);
@@ -75,9 +86,6 @@ var postToComment;
                     </li> 
 
                 </div>      <br/>`);
-                //console.log(doc.id, " => ", doc.data());
-
-
             });
 
             $(".showpost").off("click",clickHandler);
@@ -97,38 +105,20 @@ var postToComment;
 
     }
         
-
-
-    let create_post = function()
-    {
-        db.collection("posts").add({
-            title:"test",
-            desc:"hi"
-        })
-        console.log("function cretaed")
-        .then(function(docRef) {
-            console.log("Document written with ID: ", docRef.id);
-        })
-        .catch(function(error) {
-            console.error("Error adding document: ", error);
-        });
-    }
-    
-
-
         displayLobby();
-
  
     //this functions creates comments
-    function create_comment()
+    function create_comment(id)
     {
         var x= $("#commentInput").val();
-        console.log(postToComment);
+        console.log(id);
         console.log("creatting comment");
         var today = new Date();
         var currdate = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+        var time = today.getHours()%12 + ":" + today.getMinutes() + ":" + today.getSeconds();
+        currdate= currdate +", "+time;
 
-        db.collection(postToComment).add({
+        db.collection(id).add({
             
              comment:x,
              date: currdate
@@ -138,31 +128,35 @@ var postToComment;
             console.log("Document written with ID: ", docRef.id);
             //document.location.reload(true);
 
-            displaySinglePost(postToComment);
+            displaySinglePost(id);
 
         })
         .catch(function(error) {
             console.error("Error adding document: ", error);
         });
 
-
-      //  sleep(2);
     }
     
 
-    function getComments()
+
+    /*
+    this functions calls another function to display all the comments
+    for a specific post/comment
+    */
+    function getComments(id)
     {
         $("#commentDiv").html($("#comments").html());
-        db.collection(postToComment)
+        db.collection(id)
         .onSnapshot(function(doc) {
            // console.log("Current data: ", doc.data());
-          displayComments(postToComment);
+          displayComments(id);
         })
     }
 
     function displayComments(collectionId)
     {
         db.collection(collectionId).get().then(function(querySnapshot) {
+            $("#commentList").html('');
             querySnapshot.forEach(function(doc) {
                 // doc.data() is never undefined for query doc snapshots
                 var data = doc.data();
@@ -178,7 +172,7 @@ var postToComment;
                     <br/>
 
                 </div><br/>`);
-                //console.log(doc.id, " => ", doc.data());
+               
             });
 
             $(".showcomment").off("click",commentclickHandler);
@@ -189,32 +183,32 @@ var postToComment;
 
     //display a single post
     function displaySinglePost(postid){
-       // let x = firebase.database().ref(postid);
+     
         console.log(postid);
         const post = db.collection('posts').doc(postid);
         var name;   
              //THIS IS HOW YOU ACCESS EACH POST               
-      
         post.get().then(doc => {
 
             data = doc.data();
-
-            postToComment = postid;
+            var id = doc.id;
+            
+             
             $("#mainscreen").html(`
             <div id="post_div">
             <p>created on:   ${data.date}</p>
             <h1 align ="center">${data.title}</h1> <br/>
             <h2 align ="center">${data.name}</h2> 
 
-            
             <ul id="users"> </ul>
             </div>
-            <button id="comment" onclick="create_comment()"> create comment</button>
-            </div>      <br/>`);
-        });
-        getComments();
+            <button id="comment" class="create_comment" data-commentid=${id}> create comment</button>
+          </div>      <br/>`);
+         
+          $(".create_comment").off("click",createCommentClickHandler);
+          $(".create_comment").on("click",createCommentClickHandler);
+        getComments(postid);
 
-        //create_comment(postid);
 
         db.collection(postid).get().then(function(querySnapshot) {
             querySnapshot.forEach(function(doc) {
@@ -222,30 +216,42 @@ var postToComment;
                 var id = doc.id;
                 console.log(id);            
             })})
-    }
+    })
+}
 
     function displaySinglecomment(postid){
         // let x = firebase.database().ref(postid);
-         console.log(postid);
-         postToComment = postid;
+        
+        console.log(postid);
+        const post = db.collection('posts').doc(postid);
+        var name;   
+             //THIS IS HOW YOU ACCESS EACH POST               
+        post.get().then(doc => {
+
+            data = doc.data();
+            var id = doc.id;
+        console.log(postid);
          $("#mainscreen").html(`
          <div id="post_div">
          <h1 align ="center">${postid}</h1> 
          <ul id="users"> </ul>
          </div>
-           <button id="comment" onclick="create_comment()"> create comment</button>
+           <button id="comment" class= "create_comment" data-commentid=${id}> create comment</button>
          </div>      <br/>`);
- 
-         getComments();
- 
+        
+         $(".create_comment").off("click",createCommentClickHandler);
+         $(".create_comment").on("click",createCommentClickHandler);
+        
+        })
+        getComments(postid);
          //create_comment(postid);
  
-         db.collection(postid).get().then(function(querySnapshot) {
-             querySnapshot.forEach(function(doc) {
-                 var data = doc.data();
-                 var id = doc.id;
-                 console.log(id);            
-             })})
+        //  db.collection(postid).get().then(function(querySnapshot) {
+        //      querySnapshot.forEach(function(doc) {
+        //          var data = doc.data();
+        //          var id = doc.id;
+        //          console.log(id);            
+        //      })})
      }
     
    
