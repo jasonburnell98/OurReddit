da6bd90cf5ab23dcc2d512fcbd4d2dba8937a0d7
Shabir Ghulam <sghulam@udel.edu>
2019-11-11 22:25:42 -0500

    eddited div css

--- Scripts/auth.js
+++ Scripts/auth.js
@@ -3,6 +3,8 @@
 auth.onAuthStateChanged(user => {
   if (user) {
     console.log('user logged in: ', user.email);
+    console.log('user logged in: ', user.username);
+    
     
   } else {
     console.log('user logged out');
@@ -19,6 +21,10 @@ signupForm.addEventListener('submit', (e) => {
   const email = signupForm['signup-email'].value;
   const password = signupForm['signup-password'].value;
   const username= signupForm['signup-username'].value;
+  console.log(password);
+  console.log(username);
+  console.log(email);  
+
 
   // sign up the user
   auth.createUserWithEmailAndPassword(email, password).then(cred => {
