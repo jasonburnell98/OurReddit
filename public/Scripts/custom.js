a9c16237a7db9bde6d7c23c2e76fc4f96204a00e
Shabir Ghulam <sghulam@udel.edu>
2019-11-17 15:35:59 -0500

    align

--- public/Scripts/custom.js
+++ public/Scripts/custom.js
@@ -303,9 +303,9 @@ function displayuserPosts(username)
                     if(!user){
 
                         $("#theposts").append(`
-                        <div id="post_div" data-postid=${id} class="showpost"">
+                        <div id="post_div">
                         <div id="titlediv">
-                        <h1 >${data.title}</h1></div>
+                        <a data-postid=${id} class="showpost"><h1 >${data.title}</h1></a></div>
                         <small>created:   ${data.date}</small>
                         <a data-username=${data.username} class = "username">by: ${data.username}</a>
                         
@@ -318,7 +318,6 @@ function displayuserPosts(username)
                             </div>
                             </div>
         
-                            </div></div>
                          <br/>`);
                         
                      
